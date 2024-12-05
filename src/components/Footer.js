c7c316935146586428edfcbd8bf81f50cc755b7e
rafabarbato <kadeshbrt@gmail.com>
2024-12-05 00:42:17 -0300

    Update Footer component to use <footer> tag and add contact information; modify styles for improved layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,21 @@ import "../styles/styles.css";
 
 const Footer = () => {
   return (
-    <header className="footer">
+    <footer className="footer">
       <nav className="fnavbar">
         <ul className="fmenu">
           <li className="fname">Personal Fit todos os direitos reservados.</li>
+          <li>
+            <span className="fcontact">Email:</span>{" "}
+            <span className="fcontact-inside">contato@personalfit.com</span>
+          </li>
+          <li>
+            <span className="fcontact">Telefone:</span>{" "}
+            <span className="fcontact-inside">(99) 99999-9999</span>
+          </li>
         </ul>
       </nav>
-    </header>
+    </footer>
   );
 };
 
